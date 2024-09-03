22aa57ab98b14823abf3fa5c9d4b9710cec7b70f
Hossam Rakha <hossamrakha0@gmail.com>
2024-09-03 20:04:22 +0300

    instagram profile using compose UI

---
+++ app/src/main/java/com/compose/instagram_profile_screen_compose/ImageWithText.kt
@@ -0,0 +1,8 @@
+package com.compose.instagram_profile_screen_compose
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class ImageWithText(
+    val image: Painter,
+    val text: String
+)
---
+++ app/src/main/java/com/compose/instagram_profile_screen_compose/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.compose.instagram_profile_screen_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.compose.instagram_profile_screen_compose.ui.theme.Instagram_profile_screen_composeTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Instagram_profile_screen_composeTheme {
+                ProfileScreen()
+            }
+        }
+    }
+}
+
